c2fbc662a8841ffadc5b8de6dd7a1a90f5d4df90
HieuLeXuan <hieulexuan629@gmail.com>
2022-10-12 06:13:51 +0700

    add lazy loading into angular project

--- CRM-Update-Final-Project/src/app/app-routing.module.ts
+++ CRM-Update-Final-Project/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadingStrategy, RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'project',
+        loadChildren: () => import('./pages/components/project/project.module').then(m => m.ProjectModule)
+    },
+    {
+        path: 'task',
+        loadChildren: () => import('./pages/components/task/task.module').then(m => m.TaskModule)
+    },
+    {
+        path: 'user',
+        loadChildren: () => import('./pages/components/user/user.module').then(m => m.UserModule)
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {
+        preloadingStrategy: PreloadingStrategy
+    })],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
