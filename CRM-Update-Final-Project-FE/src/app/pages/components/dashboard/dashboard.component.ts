6f1d27ca72a96a62b99a90b23863eb34c7b49660
HieuLeXuan <hieulexuan629@gmail.com>
2022-11-17 20:56:20 +0700

    update ui dashboard add cursor poniter for current user login

--- CRM-Update-Final-Project-FE/src/app/pages/components/dashboard/dashboard.component.ts
+++ CRM-Update-Final-Project-FE/src/app/pages/components/dashboard/dashboard.component.ts
@@ -1,21 +1,24 @@
 import { AppSettings } from 'src/app/app.constants';
 import { ProjectService } from 'src/app/pages/services/project.service';
-import { Component, OnInit } from "@angular/core";
-import { ApexChart, ApexNonAxisChartSeries, ApexTitleSubtitle } from "ng-apexcharts";
+import { Component, OnInit } from '@angular/core';
+import {
+  ApexChart,
+  ApexNonAxisChartSeries,
+  ApexTitleSubtitle,
+} from 'ng-apexcharts';
 import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
   //Project
   projectTotal: number = 0;
   projectStatusDoing: number = 0;
   projectStatusDone: number = 0;
-  
+
   //Task
   taskTotal: number = 0;
   taskStatusUnassigned: number = 0;
@@ -24,67 +27,73 @@ export class DashboardComponent implements OnInit {
 
   constructor(
     private projectService: ProjectService,
-    private taskService: TaskService,
-  ) { }
+    private taskService: TaskService
+  ) {}
 
   ngOnInit(): void {
     //init project & task data to show charts
     this.projectService.getProjectsWithInfo().subscribe((content: any) => {
-      this.projectTotal = content.length
-      this.projectStatusDoing = content.filter((project: any) =>
-        project.status == AppSettings.PROJECT_STATUS_DOING).length
-      this.projectStatusDone = content.filter((project: any) =>
-        project.status == AppSettings.PROJECT_STATUS_DONE).length
+      this.projectTotal = content.length;
+      this.projectStatusDoing = content.filter(
+        (project: any) => project.status == AppSettings.PROJECT_STATUS_DOING
+      ).length;
+      this.projectStatusDone = content.filter(
+        (project: any) => project.status == AppSettings.PROJECT_STATUS_DONE
+      ).length;
       this.projectChartSeries = [
         this.projectStatusDoing,
         this.projectStatusDone,
-      ]
-    })
+      ];
+    });
     this.taskService.getTasksWithInfo().subscribe((content: any) => {
-      this.taskTotal = content.length
-      this.taskStatusUnassigned = content.filter((task: any) =>
-        task.status == AppSettings.TASK_STATUS_UNASSIGNED).length
-      this.taskStatusStarted = content.filter((task: any) =>
-        task.status == AppSettings.TASK_STATUS_STARTED).length
-      this.taskStatusCompleted = content.filter((task: any) =>
-        task.status == AppSettings.TASK_STATUS_COMPLETED).length
+      this.taskTotal = content.length;
+      this.taskStatusUnassigned = content.filter(
+        (task: any) => task.status == AppSettings.TASK_STATUS_UNASSIGNED
+      ).length;
+      this.taskStatusStarted = content.filter(
+        (task: any) => task.status == AppSettings.TASK_STATUS_STARTED
+      ).length;
+      this.taskStatusCompleted = content.filter(
+        (task: any) => task.status == AppSettings.TASK_STATUS_COMPLETED
+      ).length;
       this.taskChartSeries = [
         this.taskStatusUnassigned,
         this.taskStatusStarted,
         this.taskStatusCompleted,
-      ]
-    })
+      ];
+    });
   }
 
   //------------------PROJECT CHART--------------------
-  projectChartSeries: ApexNonAxisChartSeries = []
+  projectChartSeries: ApexNonAxisChartSeries = [];
   projectChartDetails: ApexChart = {
+    width: 600,
     type: 'donut',
     toolbar: {
       show: true,
-    }
-  }
-  projectChartColors = ['#0d6efd', '#198754']
+    },
+  };
+  projectChartColors = ['#0d6efd', '#198754'];
   projectChartLabels = [
-    AppSettings.PROJECT_STATUS_DOING, 
-    AppSettings.PROJECT_STATUS_DONE
-  ]
+    AppSettings.PROJECT_STATUS_DOING,
+    AppSettings.PROJECT_STATUS_DONE,
+  ];
   //----------------------------------------------------
 
-
   //------------------TASK CHART------------------------
-  taskChartSeries: ApexNonAxisChartSeries = []
+  taskChartSeries: ApexNonAxisChartSeries = [];
   taskChartDetails: ApexChart = {
+    width: 600,
     type: 'pie',
     toolbar: {
       show: true,
-    }
-  }
-  taskChartColors = ['#0dcaf0', '#0d6efd', '#198754']
+    },
+  };
+  taskChartColors = ['#0dcaf0', '#0d6efd', '#198754'];
   taskChartLabels = [
-    AppSettings.TASK_STATUS_UNASSIGNED, 
+    AppSettings.TASK_STATUS_UNASSIGNED,
     AppSettings.TASK_STATUS_STARTED,
-    AppSettings.TASK_STATUS_COMPLETED
-  ]
+    AppSettings.TASK_STATUS_COMPLETED,
+  ];
   //----------------------------------------------------
 }
